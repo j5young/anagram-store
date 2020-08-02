dad4515c1fdfacd68b55b7de0a5d9b3699bb4455
Jennifer Young Chan <jennyoung@gmail.com>
2020-08-02 19:50:49 -0400

    Updating pom.xml

--- src/main/java/com/anagram/serverless/springboot/Application.java
+++ src/main/java/com/anagram/serverless/springboot/Application.java
@@ -3,7 +3,7 @@ package com.anagram.serverless.springboot;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.HandlerAdapter;
