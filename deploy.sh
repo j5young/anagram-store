65f06753a7686bedfa300e8901090e2980256bd0
Jennifer Young Chan <jennyoung@gmail.com>
2020-08-02 10:33:42 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
 npm install -g serverless
-serverless deploy --stage dev --aws-profile devProfile --package $CODEBUILD_SRC_DIR/target/dev -v
+export AWS_ACCESS_KEY_ID=AKIAVLT2IBIG2VCFDW4C
+export AWS_SECRET_ACCESS_KEY=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
+serverless deploy --stage dev --package $CODEBUILD_SRC_DIR/target/dev -v
