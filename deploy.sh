7220f10f8fa9b74e5d7aa14d002e28e8d912d0b0
Jennifer Young Chan <jennyoung@gmail.com>
2020-08-02 10:24:42 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 npm install -g serverless
-serverless deploy --stage dev --package $CODEBUILD_SRC_DIR/target/dev -v
+serverless deploy --stage dev --aws-profile devProfile --package $CODEBUILD_SRC_DIR/target/dev -v
