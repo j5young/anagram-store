8f35973a2e31334167ce84a7e864dd0345496bf5
Jennifer Young Chan <jennyoung@gmail.com>
2020-08-02 20:45:01 -0400

    Update tests

--- src/test/java/com/anagram/serverless/springboot/test/ApplicationTests.java
+++ src/test/java/com/anagram/serverless/springboot/test/ApplicationTests.java
@@ -1,13 +1,15 @@
 package com.anagram.serverless.springboot.test;
 
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.HttpStatus;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
@@ -21,16 +23,22 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import junit.framework.*;
+import static org.junit.Assert.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-class ApplicationTests {
+@RunWith(SpringRunner.class)
+public class ApplicationTests {
 	
 	@Autowired
 	private MockMvc mockMvc;
 	
 	@Autowired
     private WebApplicationContext wac;
+	
+	public ApplicationTests() {
+		
+	}
 	
 	@Before
     public void setup () {
@@ -53,7 +61,7 @@ class ApplicationTests {
 		JSONAssert.assertEquals(expected, result.getResponse()
 				.getContentAsString(), false);
 		
-		Assert.assertEquals(MediaType.APPLICATION_JSON, response.getHeader(HttpHeaders.CONTENT_TYPE));
+		Assert.assertEquals(MediaType.APPLICATION_JSON+";charset=UTF-8", response.getHeader(HttpHeaders.CONTENT_TYPE));
 		
 		Assert.assertEquals(HttpStatus.OK.value(),response.getStatus());
 		
